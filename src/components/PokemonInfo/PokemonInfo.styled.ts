c773f553db1eccaf1084aab1960a36084b9d09e7
AngelDeCaldasD01 <angel.decaldas@driving01.com>
2023-08-23 22:56:17 +0200

    feat: :sparkles: falta por sobreponer la imagen en el fondo blanco y el bug del types0

--- src/components/PokemonInfo/PokemonInfo.styled.ts
+++ src/components/PokemonInfo/PokemonInfo.styled.ts
@@ -2,10 +2,10 @@ import {Dimensions, StyleSheet} from 'react-native';
 
 const screenWidth = Dimensions.get('window').height;
 const desiredWidth = screenWidth * 1 + 60;
-console.log(desiredWidth);
+
 const pokemonInfoStyles = StyleSheet.create({
   bg: {
-    height: '100%',
+    height: desiredWidth - 487,
     backgroundColor: 'white',
     padding: 20,
     borderTopLeftRadius: 24,
